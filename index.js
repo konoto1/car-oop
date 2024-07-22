f8ec6908ad1dc8889eb2020a6579b7d271dd923a
Tomas.K <tomas.konopackis@gmail.com>
2024-07-22 08:20:49 +0300

    oop car class homework

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Car } from "./js/Car.js/";
+
+const Automobilis = new Car('Audi', '80', 'Raudona', '68ltr', '7.6ltr/100km');
+
+console.log(Automobilis);
+console.log(Automobilis.ijungtiVarikli());
+console.log(Automobilis.pradetiVaziuoti());
+console.log(Automobilis.vaziuoti());
+console.log(Automobilis.kiekLikoKuro());
+console.log(Automobilis.sustoti());
+console.log(Automobilis.isjungtiVarikli());
+console.log(Automobilis.uzpiltiKuro());
+console.log(Automobilis);
